905a4e09f0351eb45b5cbf365d601e65eb9390df
GrandaCoder <camilo.granda@correounivalle.edu.co>
2022-12-27 16:37:18 -0500

    Taller

--- app/src/main/java/com/example/studykotlin/MainActivity.kt
+++ app/src/main/java/com/example/studykotlin/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.studykotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.ArraySet
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,34 +10,54 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        cuentaRegresiva()
 
-    //---------colecciones map:
+        //array de pares
+        var numeros:Array<Int> = arrayOf(0,1,2,3,4,5,6,7,8,9,10)
+        mostrarPares(numeros)
 
-        var mapa: Map<Int,String> = mapOf(
-            1 to "España",
-            2 to "Mexico",
-            3 to "COlombia"
-        )
-
-        println()
-        println(mapa.keys)
-
-    //------Mapa mutable
-        var inversiones:MutableMap<String,Float> = mutableMapOf()
-        inversiones.put("Cocacola", 50f)
-        inversiones.put("pepsi", 55f)
+        var plastosCarta1:Array<String> = arrayOf("salchipapa","burritos","carne gratinada")
+        recorrerPlato(plastosCarta1)
 
 
-        println(inversiones)
-
-    //-------Null safety
-        //significa que puede ser nulo o no
-        var nombre: String? = null;
-
-        if (nombre == null){
-            nombre = "Juan"
-        }
-        println(nombre)
     }
+
+    private fun recorrerPlato(platos: Array<String>) {
+        for (plato in platos)
+            println(plato)
+    }
+
+    private fun recorrerPlatoSet(platos: Set<String>) {
+        for (plato in platos)
+            println(plato)
+    }
+
+    /*Usa un bucle while para recorrer todos los números del 1 al 10 y mostrar únicamente los pares*/
+    private fun mostrarPares(numeros : Array<Int>) {
+        var i = 0
+        while (i<numeros.size){
+            if((numeros[i]%2) == 0){
+                println("Par ${numeros[i]}")
+            }
+            i++
+        }
+    }
+
+    /*Usa un bucle do-while para mostrar por pantalla una cuenta regresiva del 9 al 0
+    igual que cuando hay un lanzamiento de un cohete por parte de la NASA.*/
+    private fun cuentaRegresiva() {
+        var i: Int = 9
+        do{
+            if (i == 0) {
+                println("Despegueeee!")
+                break
+            }else {
+                println(i--)
+            }
+        }while (true)
+    }
+
+
+
 
 }
