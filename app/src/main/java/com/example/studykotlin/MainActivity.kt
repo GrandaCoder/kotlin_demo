65ae80e5b09f9a9a62a918a1d2eea826d4cab5ac
GrandaCoder <camilo.granda@correounivalle.edu.co>
2023-01-05 14:25:38 -0500

    typealias, lineas de codigo largas reducir

--- app/src/main/java/com/example/studykotlin/MainActivity.kt
+++ app/src/main/java/com/example/studykotlin/MainActivity.kt
@@ -3,28 +3,17 @@ package com.example.studykotlin
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+//se tienen que declarar aqui arriba
+typealias aliasObjeto = SubCLasses.Anidada
 
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var array4 = IntArray(5){5q} //multiplica los valores por 3 arroja 3,,6,9
-
-        //una funcion de orden superior que se le pasa una lambda que suma cada elemento,
-        // la funcion de orden superior lo que hace es que recorre cada elemento
-        var suma = 0
-        recorrerArray(array4){
-            suma += it
-        }
-
-        println("la respuesta de la suma es $suma")
-
-    }
-
-    private fun recorrerArray(array: IntArray, fn: (Int)->Unit){
-        for(i in array)
-            fn(i)
+        //ejemplo de typeAlias
+        var ani:aliasObjeto = aliasObjeto()
+        ani.presentar()
     }
 
 }
