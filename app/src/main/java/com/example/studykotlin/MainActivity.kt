c2b5f6df33d1d387e7f01618f791ca39a58ff0bc
GrandaCoder <camilo.granda@correounivalle.edu.co>
2023-01-05 14:41:46 -0500

    desestructuracion

--- app/src/main/java/com/example/studykotlin/MainActivity.kt
+++ app/src/main/java/com/example/studykotlin/MainActivity.kt
@@ -3,17 +3,26 @@ package com.example.studykotlin
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-//se tienen que declarar aqui arriba
-typealias aliasObjeto = SubCLasses.Anidada
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //ejemplo de typeAlias
-        var ani:aliasObjeto = aliasObjeto()
-        ani.presentar()
+        //Desestructuracion
+        var sol: Star = Star("Sol",2423f,"Via lactea")
+        var (nameSol, radiousSol,GalaxySun) = sol
+        println("--------$nameSol")
+
+        //si queremos saltar un parametro. aqui saltamos el radio
+        var (nameSol2, _ ,GalaxySun2) = sol
+
+        //tambien se puede sacar con algo que se llama componente
+        //aqui sacamos el parametro "name"
+        var componente = sol.component1()
+        println("Nombre del componente $componente")
+
     }
 
 }
