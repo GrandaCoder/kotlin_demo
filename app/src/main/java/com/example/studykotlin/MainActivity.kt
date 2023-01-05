552281c8846623c34f4f215d3a24837401561527
GrandaCoder <camilo.granda@correounivalle.edu.co>
2023-01-05 14:16:03 -0500

    acceso a variables externas con lambda

--- app/src/main/java/com/example/studykotlin/MainActivity.kt
+++ app/src/main/java/com/example/studykotlin/MainActivity.kt
@@ -9,17 +9,22 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //aqui podemos observar de que lo que etsá al lado de intarray es una lambda
-        var array = IntArray(8){5}
-        for (i in array)
-            println(i)
+        var array4 = IntArray(5){5q} //multiplica los valores por 3 arroja 3,,6,9
 
-        var array2 = IntArray(8){it} // it de iterador 01,2,3,....n
+        //una funcion de orden superior que se le pasa una lambda que suma cada elemento,
+        // la funcion de orden superior lo que hace es que recorre cada elemento
+        var suma = 0
+        recorrerArray(array4){
+            suma += it
+        }
 
-        var array3 = IntArray(8){it *2} //multiplica los valores por 2 arroja 2,4,8,16
+        println("la respuesta de la suma es $suma")
 
-        var array4 = IntArray(8){i -> i*3} //multiplica los valores por 3 arroja 3,,6,9
+    }
 
+    private fun recorrerArray(array: IntArray, fn: (Int)->Unit){
+        for(i in array)
+            fn(i)
     }
 
 }
