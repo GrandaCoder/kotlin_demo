409be514398e75b53df91952c272032e642f8e97
GrandaCoder <camilo.granda@correounivalle.edu.co>
2023-01-06 14:23:46 -0500

    try catch

--- app/src/main/java/com/example/studykotlin/MainActivity.kt
+++ app/src/main/java/com/example/studykotlin/MainActivity.kt
@@ -10,19 +10,46 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //Desestructuracion
-        var sol: Star = Star("Sol",2423f,"Via lactea")
-        var (nameSol, radiousSol,GalaxySun) = sol
-        println("--------$nameSol")
+        //--------- try catch finally
 
-        //si queremos saltar un parametro. aqui saltamos el radio
-        var (nameSol2, _ ,GalaxySun2) = sol
+        /*
+         Errores mas comunes
 
-        //tambien se puede sacar con algo que se llama componente
-        //aqui sacamos el parametro "name"
-        var componente = sol.component1()
-        println("Nombre del componente $componente")
+        * NullPointerException -> por ejemplo se ejecuta un objeto que no existe
+        *
+        *ArithmeticException -> por ejemplo dividir por cero
+        *
+        * Security Exception -> acceder un archivo sin permiso
+        *
+        * ArrayIndexOutBoundException -> una posicion de u array que no existe
+        *
+        * */
 
+        var a = 8
+        var b = 2
+
+        println(valueTrain(a,b))
+
+    }
+
+    private fun valueTrain(numberA:Int, numberB:Int):Any {
+
+       var respuesta = try {
+           /*
+                Lineas de código
+
+            Y por ultimo la instruccion
+           */
+           println("division $numberA / $numberB")
+
+            numberA/numberB
+
+        }catch (e:Exception){
+            // DIVISION NO PERMITIDA
+
+            "Accion no permitida"
+        }
+        return respuesta
     }
 
 }
