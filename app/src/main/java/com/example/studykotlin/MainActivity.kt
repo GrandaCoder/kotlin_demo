eee588a5c8418cfe7f156b7485b7cd3e9491d950
GrandaCoder <camilo.granda@correounivalle.edu.co>
2023-01-06 14:58:33 -0500

    scope functions, esto es lo de agrupar  codigo de un objeto por ejemplo persona.let{ codigo}

--- app/src/main/java/com/example/studykotlin/MainActivity.kt
+++ app/src/main/java/com/example/studykotlin/MainActivity.kt
@@ -10,18 +10,29 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-       var password: String = "1234"
+        var camilo: Person = Person("Camilo", "565F5",1.66F)
 
-        // entra en el throw la aplicacion da error
-        if (password.length < 6) {
-            throw ContraseniaIlegal("Constraseña muy corta")
-        }else{
-            println("Contraseña correcta")
+        //Visualemente mejor, merece la pena cuando es mucho cambios
+        camilo.let {
+            it.die()
+            it.nombre = "Juan"
         }
 
+        camilo.apply {
+            this.die()
+            this.nombre = "Pepe"
+        }.also {
+            it.passport = "NI3INR"
+        }
+
+        var maria = Person("Camilo", "565F5",1.66F).run {
+            this.die()
+            this.height = 1.54f
+
+            // no se esta creando la persona, se ejecutan ciertas cosas
+            "Este es el ultimo valor que se almacena en maria"
+        }
     }
 }
-//podemos crear nuestros propios errores.!
 
-class ContraseniaIlegal(mensaje:String) : Exception(mensaje)
 
