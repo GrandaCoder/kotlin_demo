9d807d9a4ff35006fef286ab8b3b7ee6c9f2a36b
GrandaCoder <camilo.granda@correounivalle.edu.co>
2023-01-10 15:34:42 -0500

    se agrega la fente rowdies

--- app/src/main/java/com/example/studykotlin/MainActivity.kt
+++ app/src/main/java/com/example/studykotlin/MainActivity.kt
@@ -12,56 +12,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //lazy es como para decir en caso de que no se haya inicilializado
-        val nombre: String by lazy { "Sin nombre" }
-
-        //imprimirá sin nombre
-        println(nombre)
-
-        var nombreFruta: String ="manzana"
-
-        var palabra = ""
-
-        funcionA()
-        funcionB()
-
-        if (true) {
-
-            //Borrar comentario
-            //Borrar comentario
-            //Borrar comentario
-            //Borrar comentario
-        }
-
-        //podemos  guardar lineas como favoritas
-        var favorita: String = "Ctrl + f11 o f11"
-
-
-        //para buscar la linea es mayusculas + f11
-
-        // ALT + J
-        //Quiero seleccionar ESTO
-        //Quiero seleccionar no
-        //Quiero seleccionar ESTO
-        //Quiero seleccionar no
-        //Quiero seleccionar ESTO
+        /*
+        * Cambio de fuente en res
+        * */
 
     }
 
-    //Esto se borra si presiono ctrl + Y
-
-    // si presionamos CTRL + D se duplica la linea
-    // si presionamos CTRL + D se duplica la linea
-    // si presionamos CTRL + D se duplica la linea
-    // si presionamos CTRL + D se duplica la linea
-
-    private fun funcionB() {
-        TODO("Not yet implemented")
-    }
-
-    private fun funcionA() {
-        TODO("Not yet implemented")
-    }
 }
 
 
