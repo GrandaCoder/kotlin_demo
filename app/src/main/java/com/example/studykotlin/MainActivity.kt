ca12f940295b9afe579f9b3e070a4e383de76d31
GrandaCoder <camilo.granda@correounivalle.edu.co>
2023-01-13 14:34:47 -0500

    view text ejemplo 1

--- app/src/main/java/com/example/studykotlin/MainActivity.kt
+++ app/src/main/java/com/example/studykotlin/MainActivity.kt
@@ -1,8 +1,9 @@
 package com.example.studykotlin
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import android.widget.TextView
 
 
 class MainActivity : AppCompatActivity() {
@@ -12,9 +13,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        /*
-        * Cambio de fuente en res
-        * */
+        var tvEJemplo: TextView = findViewById(R.id.tvEjemplo)
+        tvEJemplo.text = "Text cambiado desde codigo"
+        tvEJemplo.setTextColor(Color.RED)
+
 
     }
 
