33f4ae0cf1dc09ffc146392491e21774d5223653
GrandaCoder <camilo.granda@correounivalle.edu.co>
2023-01-13 14:45:17 -0500

    listeners o eventos ejemplo 2

--- app/src/main/java/com/example/studykotlin/MainActivity.kt
+++ app/src/main/java/com/example/studykotlin/MainActivity.kt
@@ -4,6 +4,7 @@ import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import android.widget.Toast
 
 
 class MainActivity : AppCompatActivity() {
@@ -13,11 +14,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //instanciamos el objeto
         var tvEJemplo: TextView = findViewById(R.id.tvEjemplo)
         tvEJemplo.text = "Text cambiado desde codigo"
         tvEJemplo.setTextColor(Color.RED)
 
-
+        //para abrir os listeners "Objeto setOnEvento"
+        tvEJemplo.setOnClickListener {
+            Toast.makeText(this,"Me hicieron click",Toast.LENGTH_SHORT).show()
+            tvEJemplo.setTextColor(Color.GREEN)
+        }
     }
 
 }
