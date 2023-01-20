9b7a7316b1eafed17770aee2f4e6cfaea992a63e
GrandaCoder <camilo.granda@correounivalle.edu.co>
2023-01-20 14:37:30 -0500

    143 calendario: calendar view

--- app/src/main/java/com/example/studykotlin/MainActivity.kt
+++ app/src/main/java/com/example/studykotlin/MainActivity.kt
@@ -5,8 +5,11 @@ import android.os.Bundle
 import android.webkit.WebSettings
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.widget.CalendarView
 import android.widget.MediaController
+import android.widget.TextView
 import android.widget.VideoView
+import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -18,11 +21,20 @@ class MainActivity : AppCompatActivity() {
 
         //widgets
 
-        var videoView = findViewById<VideoView>(R.id.videoView)
-        var mcWeb = MediaController(this)
-        mcWeb.setAnchorView(videoView)
-        videoView.setVideoPath("https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4")
-        videoView.setMediaController(mcWeb)
+        //calendar
+        var cvEjemplo = findViewById<CalendarView>(R.id.calendarView)
+        var tvFeacha = findViewById<TextView>(R.id.textView)
+
+        cvEjemplo.setOnDateChangeListener { calendarView, year, month, day ->
+            var date = "$day/${1+month}/$year"
+            tvFeacha.text = "Fecha seleccionada: $date"
+
+        }
+
+        var calendar = Calendar.getInstance()
+        calendar.set(2026,2,22)
+        cvEjemplo.date = calendar.timeInMillis
+
 
 
     }
