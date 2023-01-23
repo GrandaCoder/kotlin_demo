949facdc43eb7820ca769a03641f11fa1916a8fe
GrandaCoder <camilo.granda@correounivalle.edu.co>
2023-01-23 16:20:29 -0500

    152 divisor de ventana

--- app/src/main/java/com/example/studykotlin/MainActivity.kt
+++ app/src/main/java/com/example/studykotlin/MainActivity.kt
@@ -18,38 +18,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        // Crea un arreglo de usuarios
-        val users = arrayOf("Camilo","pepe", "Mojica", "Gustavo")
 
-        // Crea un adaptador para la lista de usuarios
-        val adapterUser: ArrayAdapter<String> = ArrayAdapter(this,android.R.layout.simple_list_item_1,users)
-
-        // Obtiene las referencias a los widgets en el layout
-        var searchView: androidx.appcompat.widget.SearchView = findViewById(R.id.searchView)
-        var lvUsers: ListView = findViewById(R.id.lvUsers)
-
-        // Asigna el adaptador a la ListView
-        lvUsers.adapter = adapterUser
-
-
-        // Establece un escucha para el SearchView
-        searchView.setOnQueryTextListener(object: androidx.appcompat.widget.SearchView.OnQueryTextListener {
-            // Método llamado cuando el usuario envía la consulta
-            override fun onQueryTextSubmit(query: String?): Boolean {
-                // Limpia el focus del SearchView
-                searchView.clearFocus()
-                // Si la lista de usuarios contiene la consulta, filtra la lista
-                if (users.contains(query)) adapterUser.filter.filter(query)
-                return false
-            }
-            // Método llamado cuando el usuario cambia la consulta
-            override fun onQueryTextChange(query: String?): Boolean {
-                // Filtra la lista de usuarios según la consulta
-                adapterUser.filter.filter(query)
-                return false
-            }
-          }
-        )
 
     }
 
